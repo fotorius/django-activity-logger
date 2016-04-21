de9611b15024b916adc49ecc5de79eaa885d0ffd
Alfredo Rius <alfredo.rius@gmail.com>
2016-04-21 14:47:54 -0500

    Upgraded version

--- activity_logger/__init__.py
+++ activity_logger/__init__.py
@@ -3,7 +3,7 @@ Django Activity Logger
 """
 
 __title__ = 'Django Activity Logger'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Alfredo Rius'
 __license__ = 'BSD 2-Clause'
 __copyright__ = 'Copyright 2016-2017 Alfredo Rius'
